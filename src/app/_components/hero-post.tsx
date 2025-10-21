73d0dd25ca93289140c0a11647afc9a3e3487552
prakashebi <45974822+prakashebi@users.noreply.github.com>
2025-10-21 13:08:39 +0530

    Remove unused props from HeroPost component

--- src/app/_components/hero-post.tsx
+++ src/app/_components/hero-post.tsx
@@ -6,25 +6,18 @@ import DateFormatter from "./date-formatter";
 
 type Props = {
   title: string;
-  coverImage: string;
-  date: string;
   excerpt: string;
-  author: Author;
   slug: string;
 };
 
 export function HeroPost({
   title,
-  coverImage,
-  date,
   excerpt,
-  author,
   slug,
 }: Props) {
   return (
     <section>
       <div className="mb-8 md:mb-16">
-        <CoverImage title={title} src={coverImage} slug={slug} />
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
@@ -37,13 +30,6 @@ export function HeroPost({
               {title}
             </Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <DateFormatter dateString={date} />
-          </div>
-        </div>
-        <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          <Avatar name={author.name} picture={author.picture} />
         </div>
       </div>
     </section>
